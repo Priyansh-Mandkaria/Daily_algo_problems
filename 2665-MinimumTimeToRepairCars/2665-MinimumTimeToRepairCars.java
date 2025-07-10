4e12171d37eefc16ed72e93f59f660e9234287e6
Priyansh Mandkaria <161031516+Priyansh-Mandkaria@users.noreply.github.com>
2025-07-10 08:52:36 +0530

    Create 2665-MinimumTimeToRepairCars

---
+++ 2665-MinimumTimeToRepairCars/2665-MinimumTimeToRepairCars.java
@@ -0,0 +1,26 @@
+// Last updated: 7/10/2025, 8:52:34 AM
+class Solution {
+    public long repairCars(int[] ranks, int cars) {
+        long left = 1, right = (long) Arrays.stream(ranks).min().getAsInt() * cars * cars;
+
+        while (left < right) {
+            long mid = (left + right) / 2;
+            if (canRepairAll(ranks, cars, mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+
+    private boolean canRepairAll(int[] ranks, int cars, long time) {
+        long totalCarsRepaired = 0;
+        for (int rank : ranks) {
+            totalCarsRepaired += Math.sqrt(time / rank);
+            if (totalCarsRepaired >= cars) return true;
+        }
+        return false;
+    }
+}
