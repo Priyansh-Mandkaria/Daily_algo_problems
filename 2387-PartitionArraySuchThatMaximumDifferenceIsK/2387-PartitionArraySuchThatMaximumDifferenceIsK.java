65ddab2110d8028d66b65355838c24a87805a892
Priyansh Mandkaria <161031516+Priyansh-Mandkaria@users.noreply.github.com>
2025-07-10 08:53:18 +0530

    Create 2387-PartitionArraySuchThatMaximumDifferenceIsK

---
+++ 2387-PartitionArraySuchThatMaximumDifferenceIsK/2387-PartitionArraySuchThatMaximumDifferenceIsK.java
@@ -0,0 +1,21 @@
+// Last updated: 7/10/2025, 8:53:15 AM
+class Solution {
+    public int partitionArray(int[] nums, int k) {
+        int max = Integer.MIN_VALUE;
+        int min = -100001;
+        for(int x : nums)
+        max = Math.max(max, x);
+        int freq[] = new int[max+1];
+        for(int x : nums){
+            freq[x]++;
+        }
+        int ans = 0; 
+        for(int i=0 ; i<=max; i++){
+            if(freq[i]>0 && i-min>k){
+                min = i; 
+                ans++; 
+            }  
+        }
+    return ans;
+    }
+}
