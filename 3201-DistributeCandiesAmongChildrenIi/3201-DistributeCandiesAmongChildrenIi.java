d5995d9a5a58ef15d9de79a502e078ba4c66c10e
Priyansh Mandkaria <161031516+Priyansh-Mandkaria@users.noreply.github.com>
2025-07-10 08:51:42 +0530

    Create 3201-DistributeCandiesAmongChildrenIi

---
+++ 3201-DistributeCandiesAmongChildrenIi/3201-DistributeCandiesAmongChildrenIi.java
@@ -0,0 +1,13 @@
+// Last updated: 7/10/2025, 8:51:39 AM
+class Solution {
+    public long distributeCandies(int n, int limit) {
+        return combCount(n)
+             - 3 * combCount(n - (limit + 1))
+             + 3 * combCount(n - 2 * (limit + 1))
+             - combCount(n - 3 * (limit + 1));
+    }
+    private long combCount(long sum) {
+        if (sum < 0) return 0;
+        return (sum + 2) * (sum + 1) / 2;
+    }
+}
