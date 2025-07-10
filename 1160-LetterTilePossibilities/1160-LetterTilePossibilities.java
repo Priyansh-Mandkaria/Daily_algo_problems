16a2983b77fc5b858b19dcf279c91dbd9423e31c
Priyansh Mandkaria <161031516+Priyansh-Mandkaria@users.noreply.github.com>
2025-07-10 08:56:19 +0530

    Create 1160-LetterTilePossibilities

---
+++ 1160-LetterTilePossibilities/1160-LetterTilePossibilities.java
@@ -0,0 +1,29 @@
+// Last updated: 7/10/2025, 8:56:16 AM
+class Solution {
+    public int numTilePossibilities(String tiles) {
+        char[] arr = tiles.toCharArray();
+        return permute(0, arr);
+    }
+    public int permute(int start, char[] arr) {
+        if(start == arr.length) return 0;
+        int ans = 0;
+        for(int i=start; i<arr.length; i++) {
+            if(!isPermutedBefore(start, i-1, arr[i], arr)) {
+                swap(start, i, arr);
+                ans += 1+permute(start+1, arr);
+                swap(start, i, arr);
+            }
+        }
+        return ans;
+    }
+    public boolean isPermutedBefore(int i, int j, char ch, char[] arr) {
+        while(i<=j)
+            if(arr[i++] == ch) return true;
+        return false;
+    }
+    public void swap(int i, int j, char[] arr) {
+        char temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
