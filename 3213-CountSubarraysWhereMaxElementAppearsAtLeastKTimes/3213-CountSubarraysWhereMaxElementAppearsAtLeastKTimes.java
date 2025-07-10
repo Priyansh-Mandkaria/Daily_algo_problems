c51adc629a7055956493a907f3401b6dafd20f40
Priyansh Mandkaria <161031516+Priyansh-Mandkaria@users.noreply.github.com>
2025-07-10 08:51:40 +0530

    Create 3213-CountSubarraysWhereMaxElementAppearsAtLeastKTimes

---
+++ 3213-CountSubarraysWhereMaxElementAppearsAtLeastKTimes/3213-CountSubarraysWhereMaxElementAppearsAtLeastKTimes.java
@@ -0,0 +1,19 @@
+// Last updated: 7/10/2025, 8:51:38 AM
+class Solution {
+    public long countSubarrays(int[] nums, int k) {
+        int maxi = Integer.MIN_VALUE;
+        for (int num : nums) maxi = Math.max(maxi, num);
+        int left = 0;
+        long maxiOccurence = 0;
+        long res = 0;
+        for (int right = 0; right < nums.length; right++) {
+            if (nums[right] == maxi) maxiOccurence++;
+            while (maxiOccurence >= k) {
+                if (nums[left] == maxi) maxiOccurence--;
+                left++;
+            }
+            res += left;
+        }
+        return res;
+    }
+}
