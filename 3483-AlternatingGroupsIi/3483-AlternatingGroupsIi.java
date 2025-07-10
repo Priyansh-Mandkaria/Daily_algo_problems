0929ef4fcc8e164db31b8bc0317d904c62be0fa6
Priyansh Mandkaria <161031516+Priyansh-Mandkaria@users.noreply.github.com>
2025-07-10 08:51:00 +0530

    Create 3483-AlternatingGroupsIi

---
+++ 3483-AlternatingGroupsIi/3483-AlternatingGroupsIi.java
@@ -0,0 +1,23 @@
+// Last updated: 7/10/2025, 8:50:57 AM
+class Solution {
+    public int numberOfAlternatingGroups(int[] colors, int k) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i: colors) list.add(i);
+        for(int i=0; i<k-1; i++) list.add(colors[i]);
+        int l = list.size();
+        int cnt = 0;
+        int i=0;
+        int j=1;
+        while(j<l) {
+            if(list.get(j) == list.get(j-1)) {
+                i = j;
+            }
+            if(j-i+1 == k) {
+                i++;
+                cnt++;
+            }
+            j++;
+        }
+        return cnt;
+    }
+}
