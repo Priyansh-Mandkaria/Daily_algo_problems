8dd55b6bd15218fb9054eb59b34f37b5771a4331
Priyansh Mandkaria <161031516+Priyansh-Mandkaria@users.noreply.github.com>
2025-07-10 08:54:30 +0530

    Create 1917-MaximumAveragePassRatio

---
+++ 1917-MaximumAveragePassRatio/1917-MaximumAveragePassRatio.java
@@ -0,0 +1,28 @@
+// Last updated: 7/10/2025, 8:54:28 AM
+class Solution {
+    public double maxAverageRatio(int[][] classes, int extraStudents) {
+        int n = classes.length;
+        PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a, b) -> 
+            Double.compare(calculateGain(classes[b[0]][0], classes[b[0]][1]),
+                           calculateGain(classes[a[0]][0], classes[a[0]][1]))
+        );
+        for (int i = 0; i < n; i++) {
+            maxHeap.offer(new int[]{i});
+        }
+        while (extraStudents-- > 0) {
+            int[] top = maxHeap.poll();
+            int index = top[0];
+            classes[index][0]++;  
+            classes[index][1]++;  
+            maxHeap.offer(new int[]{index});  
+        }
+        double totalRatio = 0.0;
+        for (int[] cls : classes) {
+            totalRatio += (double) cls[0] / cls[1];
+        }
+        return totalRatio / n;
+    }
+    private double calculateGain(int passi, int totali) {
+        return ((double) (passi + 1) / (totali + 1)) - ((double) passi / totali);
+    }
+}
