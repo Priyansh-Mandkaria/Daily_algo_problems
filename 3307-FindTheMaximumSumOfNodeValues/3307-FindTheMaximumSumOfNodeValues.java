1465d0c2d157a007ee5bbcd1753b1eaae8934d3a
Priyansh Mandkaria <161031516+Priyansh-Mandkaria@users.noreply.github.com>
2025-07-10 08:51:30 +0530

    Create 3307-FindTheMaximumSumOfNodeValues

---
+++ 3307-FindTheMaximumSumOfNodeValues/3307-FindTheMaximumSumOfNodeValues.java
@@ -0,0 +1,24 @@
+// Last updated: 7/10/2025, 8:51:27 AM
+class Solution {
+    public long maximumValueSum(int[] v, int k, int[][] edges) {
+        long total = 0;
+        for (int val : v) {
+            total += val;
+        }
+        long totalDiff = 0;
+        int positiveCount = 0;
+        long minAbsDiff = Integer.MAX_VALUE;
+        for (int val : v) {
+            long diff = (val ^ k) - val;
+            if (diff > 0) {
+                totalDiff += diff;
+                positiveCount++;
+            }
+            minAbsDiff = Math.min(minAbsDiff, Math.abs(diff));
+        }
+        if (positiveCount % 2 == 1) {
+            totalDiff -= minAbsDiff;
+        }
+        return total + totalDiff;
+    }
+}
