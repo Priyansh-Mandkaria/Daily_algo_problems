a822f8427f69ad2dc57cb5665326df091f691d40
Priyansh Mandkaria <161031516+Priyansh-Mandkaria@users.noreply.github.com>
2025-07-10 08:49:47 +0530

    Create 3754-MaximumManhattanDistanceAfterKChanges

---
+++ 3754-MaximumManhattanDistanceAfterKChanges/3754-MaximumManhattanDistanceAfterKChanges.java
@@ -0,0 +1,30 @@
+// Last updated: 7/10/2025, 8:49:45 AM
+class Solution {
+    private int solve(char a, char b, String s, int k) {
+        int dist = 0, mx = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ch == a || ch == b) {
+                dist++;
+            } else {
+                if (k > 0) {
+                    k--;
+                    dist++;
+                } else {
+                    mx = Math.max(mx, dist);
+                    dist--;
+                }
+            }
+        }
+        mx = Math.max(mx, dist);
+        return mx;
+    }
+    public int maxDistance(String s, int k) {
+        if (k == s.length()) return k;
+        int nw = solve('N', 'W', s, k);
+        int ne = solve('N', 'E', s, k);
+        int sw = solve('S', 'W', s, k);
+        int se = solve('S', 'E', s, k);
+        return Math.max(Math.max(nw, ne), Math.max(sw, se));
+    }
+}
