412c89e4c6e976fe2dd8d3e6a0293f2e376036fc
Priyansh Mandkaria <161031516+Priyansh-Mandkaria@users.noreply.github.com>
2025-07-10 08:50:11 +0530

    Create 3656-MinimumNumberOfOperationsToMakeElementsInArrayDistinct

---
+++ 3656-MinimumNumberOfOperationsToMakeElementsInArrayDistinct/3656-MinimumNumberOfOperationsToMakeElementsInArrayDistinct.java
@@ -0,0 +1,20 @@
+// Last updated: 7/10/2025, 8:50:08 AM
+class Solution {
+    public int minimumOperations(int[] nums) {
+        int freq[] = new int[101];
+        for (int i = 0; i < nums.length; i++) {
+            freq[nums[i]]++;
+        }
+        int count = 0;
+        int numPoint = 0;
+        for (int i = 0; i < 101; i++) {
+            if (freq[i] > 1) {
+                for (int j = 0; j < 3 && numPoint < nums.length; j++)
+                    freq[nums[numPoint++]]--;
+                count++;
+                i--;
+            }
+        }
+        return count;
+    }
+}
