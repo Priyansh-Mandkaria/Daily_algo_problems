b3bfff91c4b23ab1b3074467168e2ac6b2770cfd
Priyansh Mandkaria <161031516+Priyansh-Mandkaria@users.noreply.github.com>
2025-07-10 08:57:10 +0530

    Create 719-FindKThSmallestPairDistance

---
+++ 719-FindKThSmallestPairDistance/719-FindKThSmallestPairDistance.java
@@ -0,0 +1,33 @@
+// Last updated: 7/10/2025, 8:57:07 AM
+class Solution {
+    public int smallestDistancePair(int[] nums, int k) {
+        Arrays.sort(nums);
+        int left = 0;
+        int right = nums[nums.length - 1] - nums[0];
+        
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            int count = countPairs(nums, mid);
+            
+            if (count < k) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        
+        return left;
+    }
+    
+    private int countPairs(int[] nums, int mid) {
+        int count = 0;
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
+            while (j < nums.length && nums[j] - nums[i] <= mid) {
+                j++;
+            }
+            count += j - i - 1;
+        }
+        return count; 
+    }
+}
