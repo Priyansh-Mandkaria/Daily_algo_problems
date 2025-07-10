fbfd9f2f4ab69a689953ed030a8e94ac8435c03b
Priyansh Mandkaria <161031516+Priyansh-Mandkaria@users.noreply.github.com>
2025-07-10 08:59:03 +0530

    Create 70-ClimbingStairs

---
+++ 70-ClimbingStairs/70-ClimbingStairs.java
@@ -0,0 +1,16 @@
+// Last updated: 7/10/2025, 8:59:01 AM
+class Solution {
+    public int climbStairs(int n) {
+            if(n==0 || n==1){
+                n = 1;
+            return 1;
+            }
+            int a = 1, b = 1;
+            for (int i = 2; i <= n; i++) {
+            int temp = a + b;
+            a = b;
+            b = temp;
+            }
+        return b;
+    }
+}
