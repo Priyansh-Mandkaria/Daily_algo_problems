70e3dea50176b2b0727aedfe36fe9000b98be44a
Priyansh Mandkaria <161031516+Priyansh-Mandkaria@users.noreply.github.com>
2025-07-10 08:53:25 +0530

    Create 2292-CountingWordsWithAGivenPrefix

---
+++ 2292-CountingWordsWithAGivenPrefix/2292-CountingWordsWithAGivenPrefix.java
@@ -0,0 +1,12 @@
+// Last updated: 7/10/2025, 8:53:22 AM
+class Solution {
+    public int prefixCount(String[] words, String pref) {
+        int count = 0;
+        for (String word : words) {
+            if (word.startsWith(pref)) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
