668ed0f96d5b979e271b0dfbfe73d72607d9a1c4
Priyansh Mandkaria <161031516+Priyansh-Mandkaria@users.noreply.github.com>
2025-07-10 08:49:32 +0530

    Create 3805-MaximizeActiveSectionWithTradeI

---
+++ 3805-MaximizeActiveSectionWithTradeI/3805-MaximizeActiveSectionWithTradeI.java
@@ -0,0 +1,17 @@
+// Last updated: 7/10/2025, 8:49:29 AM
+class Solution{
+int maxActiveSectionsAfterTrade(String s) {
+    int oneCount = 0, convertedOne = 0, curZeroCount = 0, lastZeroCount = 0;
+    for(int i = 0; i < s.length(); ++i){
+        if(s.charAt(i) == '0') curZeroCount++;
+        else{
+            if(curZeroCount != 0) lastZeroCount = curZeroCount;
+            curZeroCount = 0;
+            oneCount++;
+        }
+        convertedOne = Math.max(convertedOne, curZeroCount + lastZeroCount);
+    }
+    if(convertedOne == curZeroCount || convertedOne == lastZeroCount) return oneCount;    
+    return oneCount + convertedOne;
+}
+}
