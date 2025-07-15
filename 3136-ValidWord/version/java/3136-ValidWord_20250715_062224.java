891172ada856ac0c7a89253079f528994e7eef02
Priyansh Mandkaria <161031516+Priyansh-Mandkaria@users.noreply.github.com>
2025-07-15 06:22:27 +0530

    Create 3136-ValidWord

---
+++ 3136-ValidWord/version/java/3136-ValidWord_20250715_062224.java
@@ -0,0 +1,36 @@
+// Last updated: 7/15/2025, 6:22:24 AM
+class Solution {
+    public boolean isValid(String word) {
+        int len=word.length();
+        if(len<3)
+        {
+            return false;
+        }
+        int count=0,count1=0;
+        for(int i=0;i<len;i++)
+        {
+            char c=word.charAt(i);
+            if((c>='a' && c<='z') || (c>='A' && c<='Z'))
+            {
+                c=Character.toLowerCase(c);
+                if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+                {
+                    count++;
+                }
+                else
+                {
+                    count1++;
+                }
+            }
+            else if(c>='0' && c<='9')
+            {
+                continue;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        return count > 0 && count1 > 0;
+    }
+}
