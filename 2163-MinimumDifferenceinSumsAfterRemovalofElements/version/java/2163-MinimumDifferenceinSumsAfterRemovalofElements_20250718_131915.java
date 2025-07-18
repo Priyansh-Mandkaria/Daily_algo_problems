4406b897454a3eb39342d14f0e169804c342ac82
Priyansh Mandkaria <161031516+Priyansh-Mandkaria@users.noreply.github.com>
2025-07-18 13:19:17 +0530

    Create 2163-MinimumDifferenceinSumsAfterRemovalofElements

---
+++ 2163-MinimumDifferenceinSumsAfterRemovalofElements/version/java/2163-MinimumDifferenceinSumsAfterRemovalofElements_20250718_131915.java
@@ -0,0 +1,41 @@
+// Last updated: 7/18/2025, 1:19:15 PM
+class Solution {
+    public long minimumDifference(int[] nums) {
+        int n = nums.length / 3;
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        long[] maxSum = new long[n + 2];
+        long sum = 0;
+        for (int i = 2 * n; i < 3 * n; i++) {
+            minHeap.offer(nums[i]);
+            sum += nums[i];
+        }
+        maxSum[n + 1] = sum;
+        for (int i = 2 * n - 1; i >= n; i--) {
+            minHeap.offer(nums[i]);
+            sum += nums[i];
+            int removed = minHeap.poll();
+            sum -= removed;
+            maxSum[i - n + 1] = sum;
+        }
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        long[] minSum = new long[n + 2];
+        sum = 0;
+        for (int i = 0; i < n; i++) {
+            maxHeap.offer(nums[i]);
+            sum += nums[i];
+        }
+        minSum[0] = sum;
+        for (int i = n; i < 2 * n; i++) {
+            maxHeap.offer(nums[i]);
+            sum += nums[i];
+            int removed = maxHeap.poll();
+            sum -= removed;
+            minSum[i - n + 1] = sum;
+        }
+        long ans = Long.MAX_VALUE;
+        for (int i = 0; i <= n; i++) {
+            ans = Math.min(ans, minSum[i] - maxSum[i + 1]);
+        }
+        return ans;
+    }
+}
