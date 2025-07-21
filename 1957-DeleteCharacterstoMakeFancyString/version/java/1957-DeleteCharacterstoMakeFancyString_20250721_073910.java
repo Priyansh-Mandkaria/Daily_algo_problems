fc84a1ada9be86acf8ae3e3f91de9f2f32892850
Priyansh Mandkaria <161031516+Priyansh-Mandkaria@users.noreply.github.com>
2025-07-21 07:39:13 +0530

    Create 1957-DeleteCharacterstoMakeFancyString

---
+++ 1957-DeleteCharacterstoMakeFancyString/version/java/1957-DeleteCharacterstoMakeFancyString_20250721_073910.java
@@ -0,0 +1,19 @@
+// Last updated: 7/21/2025, 7:39:10 AM
+class Solution {
+    public String makeFancyString(String s) {
+        StringBuilder res = new StringBuilder();
+        int count = 1;
+        res.append(s.charAt(0));
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == s.charAt(i - 1)) {
+                count++;
+            } else {
+                count = 1;
+            }
+            if (count < 3) {
+                res.append(s.charAt(i));
+            }
+        }
+        return res.toString();
+    }
+}
