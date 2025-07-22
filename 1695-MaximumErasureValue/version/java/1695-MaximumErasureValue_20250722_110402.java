394dc353c964c7c60603568ae995e61dcabe0786
Priyansh Mandkaria <161031516+Priyansh-Mandkaria@users.noreply.github.com>
2025-07-22 11:04:07 +0530

    Create 1695-MaximumErasureValue

---
+++ 1695-MaximumErasureValue/version/java/1695-MaximumErasureValue_20250722_110402.java
@@ -0,0 +1,18 @@
+// Last updated: 7/22/2025, 11:04:02 AM
+class Solution {
+    public int maximumUniqueSubarray(int[] nums) {
+        int result = 0, currentSum = 0, start = 0;
+        Set<Integer> seen = new HashSet<>();
+        for (int end = 0; end < nums.length; end++) {
+            while (seen.contains(nums[end])) {
+                seen.remove(nums[start]);
+                currentSum -= nums[start];
+                start++;
+            }
+            currentSum += nums[end];
+            seen.add(nums[end]);
+            result = Math.max(result, currentSum);
+        }
+        return result;
+    }
+}
