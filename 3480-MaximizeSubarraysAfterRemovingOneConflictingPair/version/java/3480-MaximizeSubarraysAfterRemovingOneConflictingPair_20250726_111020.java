a818ff3e3793c4cf65fda04b0dc8732fba0db567
Priyansh Mandkaria <161031516+Priyansh-Mandkaria@users.noreply.github.com>
2025-07-26 11:10:23 +0530

    Create 3480-MaximizeSubarraysAfterRemovingOneConflictingPair

---
+++ 3480-MaximizeSubarraysAfterRemovingOneConflictingPair/version/java/3480-MaximizeSubarraysAfterRemovingOneConflictingPair_20250726_111020.java
@@ -0,0 +1,42 @@
+// Last updated: 7/26/2025, 11:10:20 AM
+class Solution {
+    public long maxSubarrays(int n, int[][] conflictingPairs) {
+
+        List<Integer>[] right = new ArrayList[n + 1];
+        for (int i = 0; i <= n; i++) {
+            right[i] = new ArrayList<>();
+        }
+
+        for (int[] x : conflictingPairs) {
+            right[Math.max(x[0], x[1])].add(Math.min(x[0], x[1]));
+        }
+
+        
+        long ans = 0;              
+        int maxleft = 0;
+        int secmaxleft = 0; 
+        long[] imp = new long[n + 1]; 
+        long add = 0;
+        
+        for (int r = 1; r <= n; r++) {
+            
+            for (int l : right[r]) {
+
+                if (l > maxleft) {
+                    int temp = maxleft;
+                    maxleft = l;
+                    secmaxleft = temp;
+                } else {
+                    secmaxleft = Math.max(secmaxleft, l);
+                }
+            }
+
+            ans += (long) (r - maxleft);
+            imp[maxleft] += (long) (maxleft - secmaxleft);
+            add = Math.max(add, imp[maxleft]);
+        }
+
+       
+        return ans + add;
+    }
+}
