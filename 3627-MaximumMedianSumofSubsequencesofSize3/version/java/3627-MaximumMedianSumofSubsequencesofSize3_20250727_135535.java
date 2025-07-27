82ae566247600007c5180018463881c12b85685e
Priyansh Mandkaria <161031516+Priyansh-Mandkaria@users.noreply.github.com>
2025-07-27 13:55:39 +0530

    Create 3627-MaximumMedianSumofSubsequencesofSize3

---
+++ 3627-MaximumMedianSumofSubsequencesofSize3/version/java/3627-MaximumMedianSumofSubsequencesofSize3_20250727_135535.java
@@ -0,0 +1,10 @@
+// Last updated: 7/27/2025, 1:55:35 PM
+class Solution {
+    public long maximumMedianSum(int[] arr) {
+        Arrays.sort(arr);
+        long sum = 0;
+        int d = arr.length / 3;
+        for (int i = d; i < arr.length; i += 2) sum += arr[i];
+        return sum;
+    }
+}
