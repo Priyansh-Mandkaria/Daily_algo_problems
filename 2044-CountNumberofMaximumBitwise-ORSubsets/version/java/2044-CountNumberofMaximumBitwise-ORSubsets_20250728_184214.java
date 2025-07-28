83913e04012253bb59acdc938796f7f93299b089
Priyansh Mandkaria <161031516+Priyansh-Mandkaria@users.noreply.github.com>
2025-07-28 18:42:19 +0530

    Create 2044-CountNumberofMaximumBitwise-ORSubsets

---
+++ 2044-CountNumberofMaximumBitwise-ORSubsets/version/java/2044-CountNumberofMaximumBitwise-ORSubsets_20250728_184214.java
@@ -0,0 +1,14 @@
+// Last updated: 7/28/2025, 6:42:14 PM
+class Solution {
+    public int countMaxOrSubsets(int[] nums) {
+        int maxOr = 0;
+        for (int n : nums) maxOr |= n;
+        return dfs(nums, 0, 0, maxOr);
+    }
+    private int dfs(int[] nums, int index, int currOr, int maxOr) {
+        if (index == nums.length) return currOr == maxOr ? 1 : 0;
+        int include = dfs(nums, index + 1, currOr | nums[index], maxOr);
+        int exclude = dfs(nums, index + 1, currOr, maxOr);
+        return include + exclude;
+    }
+}
