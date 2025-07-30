f7e137620b263aee6578d6f834d2c5aa18742fed
Priyansh Mandkaria <161031516+Priyansh-Mandkaria@users.noreply.github.com>
2025-07-30 23:24:11 +0530

    Create 875-KokoEatingBananas

---
+++ 875-KokoEatingBananas/version/java/875-KokoEatingBananas_20250730_232402.java
@@ -0,0 +1,30 @@
+// Last updated: 7/30/2025, 11:24:02 PM
+import java.util.*;
+
+class Solution {
+    public int minEatingSpeed(int[] arr, int h) {
+        int low = 1; 
+        int high = Arrays.stream(arr).max().getAsInt();
+        int ans = -1; 
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (koko(arr, mid) <= h) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return ans; 
+    }
+
+    private long koko(int[] arr, int speed) {
+        long time = 0;
+        for (int bananas : arr) {
+            time += (bananas + (long)speed - 1) / speed;
+        }
+        return time;
+    }
+}
