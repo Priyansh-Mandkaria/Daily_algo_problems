1cad132d2a9d6711b5f0a4dc93382b65e9adb9c4
Priyansh Mandkaria <161031516+Priyansh-Mandkaria@users.noreply.github.com>
2025-07-31 22:18:35 +0530

    Create 1482-MinimumNumberofDaystoMakemBouquets

---
+++ 1482-MinimumNumberofDaystoMakemBouquets/version/java/1482-MinimumNumberofDaystoMakemBouquets_20250731_221826.java
@@ -0,0 +1,36 @@
+// Last updated: 7/31/2025, 10:18:26 PM
+class Solution {
+    public int minDays(int[] arr, int m, int k) {
+        if ((long) k * m > arr.length) return -1;
+        int ans = -1;
+        int low = Arrays.stream(arr).min().getAsInt();
+        int high = Arrays.stream(arr).max().getAsInt();
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if(possible(arr,mid,k,m) == true){
+                ans = mid;
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1; 
+            }
+        }
+        return ans;
+    }
+    private boolean possible(int[] arr, int a , int k , int m){
+        int cnt = 0;
+        int cnt2 = 0;
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] <= a){
+                cnt++;
+            }
+            else{
+                cnt2 += cnt/k;
+                cnt = 0;
+            }
+        }
+        cnt2 += cnt/k;
+        if(cnt2 >= m) return true;
+        return false;
+    }
+}
