468427baaf06ffea1cc0f2c6e56b2fe5c3bccb1f
Priyansh Mandkaria <161031516+Priyansh-Mandkaria@users.noreply.github.com>
2025-10-28 12:47:32 +0530

    Create 1859-SortingtheSentence

---
+++ 1859-SortingtheSentence/version/java/1859-SortingtheSentence_20251028_124726.java
@@ -0,0 +1,11 @@
+// Last updated: 10/28/2025, 12:47:26 PM
+class Solution {
+    public String sortSentence(String s) {
+        String[] words = s.split(" ");
+        String[] ans = new String[words.length];
+        for (String c: words){
+            ans[(int)(c.charAt(c.length() - 1) - '1')] = c.substring(0, c.length() - 1);
+        }
+        return String.join(" ", ans);
+    }
+}
