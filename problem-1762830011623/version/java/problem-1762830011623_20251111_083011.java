fd27fbd4f9d017af164760c7b84d8ef8e0d245c5
Priyansh Mandkaria <161031516+Priyansh-Mandkaria@users.noreply.github.com>
2025-11-11 08:30:17 +0530

    Create

---
+++ problem-1762830011623/version/java/problem-1762830011623_20251111_083011.java
@@ -0,0 +1,26 @@
+// Last updated: 11/11/2025, 8:30:11 AM
+class Solution {
+    private int[] getCnts(String s) {
+        int zeros = 0;
+        int ones = 0;
+        for (char ch : s.toCharArray()) {
+            if (ch == '0') zeros++;
+            else ones++;
+        }
+        return new int[] {zeros , ones};
+    }
+    public int findMaxForm(String[] strs, int m, int n) {
+        int[][] dp = new int[m + 1][n + 1];
+        for (String s : strs) {
+            int[] cost = getCnts(s);
+            int cst0 = cost[0];
+            int cst1 = cost[1];
+            for (int i = m; i >= cst0; i--) {
+                for (int j = n; j >= cst1; j--) {
+                    dp[i][j] = Math.max(dp[i][j] , 1 + dp[i - cst0][j - cst1]);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}
