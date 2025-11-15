6ef1e4b562d9ec55bbaea828a0e53ad69ebb51fe
Priyansh Mandkaria <161031516+Priyansh-Mandkaria@users.noreply.github.com>
2025-11-15 18:14:16 +0530

    Create 1309-DecryptStringfromAlphabettoIntegerMapping

---
+++ 1309-DecryptStringfromAlphabettoIntegerMapping/version/java/1309-DecryptStringfromAlphabettoIntegerMapping_20251115_181406.java
@@ -0,0 +1,26 @@
+// Last updated: 11/15/2025, 6:14:06 PM
+class Solution {
+    public String freqAlphabets(String s) {
+        Map<Integer,Character> mp = new HashMap<>();
+        for(char i = 'a'; i <= 'z'; i++){
+            mp.put(i - 'a' + 1, i);
+        }
+        StringBuilder sb = new StringBuilder();
+        int i = s.length() - 1;
+        while(i >= 0){
+            if(s.charAt(i) == '#'){
+                String sub = s.substring(i-2,i);
+                int num = Integer.parseInt(sub);
+                sb.append(mp.get(num));
+                i = i -3;
+            }
+            else{
+                char sub = s.charAt(i);
+                int num = Character.getNumericValue(sub);
+                sb.append(mp.get(num));
+                i = i - 1;
+            }
+        }
+        return sb.reverse().toString();
+    }
+}
