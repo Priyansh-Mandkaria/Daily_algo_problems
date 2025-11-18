4a93439e6b164e49acf07c3a98aee7948252927a
Priyansh Mandkaria <161031516+Priyansh-Mandkaria@users.noreply.github.com>
2025-11-18 17:55:45 +0530

    Create 717-1-bitand2-bitCharacters

---
+++ 717-1-bitand2-bitCharacters/version/java/717-1-bitand2-bitCharacters_20251118_175542.java
@@ -0,0 +1,11 @@
+// Last updated: 11/18/2025, 5:55:42 PM
+class Solution {
+    public boolean isOneBitCharacter(int[] bits) {
+        final int n = bits.length;
+        int i = 0;
+        while(i < n - 1){
+            i += 1 + bits[i];
+        }
+        return i == n-1;
+    }
+}
