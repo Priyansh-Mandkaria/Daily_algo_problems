3021bf58e44d406075bc71e75657a47ecfe60e69
Priyansh Mandkaria <161031516+Priyansh-Mandkaria@users.noreply.github.com>
2025-11-19 14:19:40 +0530

    Create 2154-KeepMultiplyingFoundValuesbyTwo

---
+++ 2154-KeepMultiplyingFoundValuesbyTwo/version/java/2154-KeepMultiplyingFoundValuesbyTwo_20251119_141937.java
@@ -0,0 +1,14 @@
+// Last updated: 11/19/2025, 2:19:37 PM
+class Solution {
+    public int findFinalValue(int[] nums, int k) {
+        int bits = 0;
+        for (int num : nums) {
+            if (num % k != 0) continue;
+            int n = num / k;
+            if ((n & (n - 1)) == 0)
+                bits |= n;
+        }
+        bits++;
+        return k * (bits & -bits);
+    }
+}
