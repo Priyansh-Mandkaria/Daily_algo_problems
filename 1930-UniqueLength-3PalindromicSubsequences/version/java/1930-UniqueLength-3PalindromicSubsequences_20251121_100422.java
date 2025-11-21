83ed7222c2d29061be556ecfe5d0af7d370cac32
Priyansh Mandkaria <161031516+Priyansh-Mandkaria@users.noreply.github.com>
2025-11-21 10:04:27 +0530

    Create 1930-UniqueLength-3PalindromicSubsequences

---
+++ 1930-UniqueLength-3PalindromicSubsequences/version/java/1930-UniqueLength-3PalindromicSubsequences_20251121_100422.java
@@ -0,0 +1,28 @@
+// Last updated: 11/21/2025, 10:04:22 AM
+class Solution {
+    public int countPalindromicSubsequence(String s) {
+        int n = s.length();
+        int[][] left = new int[n][26]; 
+        int[][] right = new int[n][26];
+        int result = 0;
+        for (int i = 1; i < n; i++) {
+            System.arraycopy(left[i - 1], 0, left[i], 0, 26);
+            left[i][s.charAt(i - 1) - 'a']++;
+        }
+        for (int i = n - 2; i >= 0; i--) {
+            System.arraycopy(right[i + 1], 0, right[i], 0, 26);
+            right[i][s.charAt(i + 1) - 'a']++;
+        }
+        boolean[][] seen = new boolean[26][26];
+        for (int j = 1; j < n - 1; j++) {
+            int middleChar = s.charAt(j) - 'a';
+            for (int ch = 0; ch < 26; ch++) {
+                if (left[j][ch] > 0 && right[j][ch] > 0 && !seen[ch][middleChar]) {
+                    result++;
+                    seen[ch][middleChar] = true; 
+                }
+            }
+        }
+        return result;
+    }
+}
