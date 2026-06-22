015f6cfd8db671082efa879cdeb8f94594220fc3
Priyansh Mandkaria <161031516+Priyansh-Mandkaria@users.noreply.github.com>
2026-06-22 13:36:40 +0530

    Create 3794-FindTheMinimumAmountOfTimeToBrewPotions

---
+++ 3794-FindTheMinimumAmountOfTimeToBrewPotions/3794-FindTheMinimumAmountOfTimeToBrewPotions.java
@@ -0,0 +1,16 @@
+// Last updated: 6/22/2026, 1:36:38 PM
+class Solution {
+    public long minTime(int[] skill, int[] mana) {
+        int n = skill.length, m = mana.length;
+        long[] done = new long[n + 1];
+        for (int j = 0; j < m; ++j) {
+            for (int i = 0; i < n; ++i) {
+                done[i + 1] = Math.max(done[i + 1], done[i]) + (long) mana[j] * skill[i];
+            }
+            for (int i = n - 1; i > 0; --i) {
+                done[i] = done[i + 1] - (long) mana[j] * skill[i];
+            }
+        }
+        return done[n];
+    }
+}
