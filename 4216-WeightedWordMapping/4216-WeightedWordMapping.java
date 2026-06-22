b49d54c5553cfcb08f0ae7278bafd9127519d162
Priyansh Mandkaria <161031516+Priyansh-Mandkaria@users.noreply.github.com>
2026-06-22 13:34:03 +0530

    Create 4216-WeightedWordMapping

---
+++ 4216-WeightedWordMapping/4216-WeightedWordMapping.java
@@ -0,0 +1,15 @@
+// Last updated: 6/22/2026, 1:34:00 PM
+class Solution {
+    public String mapWordWeights(String[] words, int[] weights) {
+        StringBuilder ans = new StringBuilder();
+        for (String word : words) {
+            long sum = 0;
+            for (char ch : word.toCharArray()) {
+                sum += weights[ch - 'a'];
+            }
+            int remainder = (int)(sum % 26);
+            ans.append((char)('z' - remainder));
+        }
+        return ans.toString();
+    }
+}
