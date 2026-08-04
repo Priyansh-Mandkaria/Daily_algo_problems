664a899eb28ebb80f68bc024f748dd6a73aa5b33
Priyansh Mandkaria <161031516+Priyansh-Mandkaria@users.noreply.github.com>
2026-08-04 09:25:14 +0530

    Create 3731-FindMissingElements

---
+++ 3731-FindMissingElements/version/java/3731-FindMissingElements_20260804_092513.java
@@ -0,0 +1,20 @@
+// Last updated: 8/4/2026, 9:25:13 AM
+1class Solution {
+2    public List<Integer> findMissingElements(int[] nums) {
+3        int min = Integer.MAX_VALUE;
+4        int max = Integer.MIN_VALUE;
+5        List<Integer> hi = new ArrayList<>();
+6        for(int num : nums){
+7            if(num < min)min = num;
+8            if(num > max)max = num;
+9            hi.add(num);
+10        }
+11        List<Integer> ni = new ArrayList<>();
+12        for(int i = min; i < max+1; i++ ){
+13            if(!hi.contains(i)){
+14                ni.add(i);
+15            }
+16        }
+17        return ni;
+18    }
+19}
